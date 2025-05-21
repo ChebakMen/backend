172c1b35090ce111096d6d424bf339471dcb9b42
ChebakMen <132047867+ChebakMen@users.noreply.github.com>
2025-05-21 13:57:09 +0700

    vecel_test6

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
 app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('view engine', 'jade');
 
 //Статика из 'uploads'
 app.use('/uploads', express.static('uploads'));
